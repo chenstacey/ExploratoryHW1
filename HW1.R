b9e8ac64d7aed0eb6b2dd634e4225ee4c436a1d8
Stacey Chen <61345014+chenstacey@users.noreply.github.com>
2020-03-11 17:29:14 +0800

    HW1 - Loading data

---
+++ HW1.R
@@ -0,0 +1,14 @@
+#Loading the data
+library("readr")
+t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+dateTime <- paste(t$Date, t$Time)
+dateTime <- setNames(dateTime, "DateTime")
+t <- t[ ,!(names(t) %in% c("Date","Time"))]
+t <- cbind(dateTime, t)
+t$dateTime <- as.POSIXct(dateTime)
+
+
+#Making plots
